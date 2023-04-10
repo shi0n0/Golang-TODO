e10d46138ad2bb120113c67b8f4e79fbd85e0328
shi0n0 <shiono.yui1080@gmail.com>
2023-04-10 23:56:00 +0900

    Echo Template Rendering

--- go.mod
+++ go.mod
@@ -2,8 +2,11 @@ module github.com/shi0n0/Golang-TODO
 
 go 1.19
 
+require github.com/labstack/echo/v4 v4.10.2
+
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -14,4 +17,5 @@ require (
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/shi0n0/Golang-TODO/handler"
 )
 
+var e = createMux()
+
+func createMux() *echo.Echo {
+    e := echo.New()
+    return e
+}
+
+type TemplateRender struct {
+	templates *template.Template
+}
+
+func (t *TemplateRender) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":8000"))
+	http.Handle("/", e)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.Gzip())
+
+	render := &TemplateRender{
+		templates: template.Must(template.ParseGlob("view/*.html")),
+	}
+	e.Renderer = render
+
+	e.GET("/", handler.ShowHTML)
+	
+	e.Start(":8000")
 }
