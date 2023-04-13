c72004fab4cb770982abbb265135ab874f0c7ffa
shi0n0 <shiono.yui1080@gmail.com>
2023-04-13 23:56:35 +0900

    Added saving database

--- database/db.go
+++ database/db.go
@@ -1,35 +1,44 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
+	"database/sql"
+	"fmt"
+    "net/http"
 
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo/v4"
 )
 
-func main() {
+func connect() (*sql.DB, error) {
     // [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
     dbconf := "todo:password@tcp(127.0.0.1:3306)/todo?charset=utf8mb4"
 
     db, err := sql.Open("mysql", dbconf)
 
     if err != nil {
-        fmt.Println(err.Error())
-        return
+        return nil, err
     }
+    if err := db.Ping(); err != nil {
+        return nil, err
+    }
+    return db, nil
+}
 
-    defer func() {
-        if err := db.Close(); err != nil {
-            fmt.Println(err.Error())
-        }
-    }()
+func saveData(c echo.Context) error {
+    text := c.FormValue("text")
 
-    err = db.Ping()
-
+    db,err := connect()
     if err != nil {
-        fmt.Println("データベース接続失敗")
-        return
+        fmt.Println(err)
+        return c.String(http.StatusInternalServerError, "保存までにエラーが発生しました")
+    }
+    defer db.Close()
+
+    query := "INSERT INTO users (name, age) VALUES (?, ?)"
+    if _ ,err := db.Exec(query, text); err != nil {
+        fmt.Println(err)
+        return c.String(http.StatusInternalServerError, "保存までにエラーが発生しました")
     }
 
-    fmt.Println("データベース接続成功")
+    return c.String(http.StatusOK, "保存完了")
 }
