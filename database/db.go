f8f8faf7d34e6400c3bc745e5b143da2681e60c7
shi0n0 <shiono.yui1080@gmail.com>
2023-04-14 01:13:51 +0900

    create Table

--- database/db.go
+++ database/db.go
@@ -1,4 +1,4 @@
-package main
+package database
 
 import (
 	"database/sql"
@@ -9,7 +9,31 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func connect() (*sql.DB, error) {
+func CreateTable() error {
+    db, err := Connect()
+    if err != nil {
+        return err
+    }
+    defer db.Close()
+
+    // テーブル作成のクエリー
+    query := `CREATE TABLE IF NOT EXISTS your_table_name (
+                    id INT NOT NULL AUTO_INCREMENT,
+                    content VARCHAR(255) NOT NULL,
+                    PRIMARY KEY (id)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;`
+
+    // クエリーを実行
+    _, err = db.Exec(query)
+    if err != nil {
+        return err
+    }
+
+    fmt.Println("テーブルが正常に作られました")
+    return nil
+}
+
+func Connect() (*sql.DB, error) {
     // [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
     dbconf := "todo:password@tcp(127.0.0.1:3306)/todo?charset=utf8mb4"
 
@@ -24,18 +48,18 @@ func connect() (*sql.DB, error) {
     return db, nil
 }
 
-func saveData(c echo.Context) error {
-    text := c.FormValue("text")
+func SaveData(c echo.Context) error {
+    content := c.FormValue("text")
 
-    db,err := connect()
+    db,err := Connect()
     if err != nil {
         fmt.Println(err)
         return c.String(http.StatusInternalServerError, "保存までにエラーが発生しました")
     }
     defer db.Close()
 
-    query := "INSERT INTO users (name, age) VALUES (?, ?)"
-    if _ ,err := db.Exec(query, text); err != nil {
+    query := "INSERT INTO users (text) VALUES (?)"
+    if _ ,err := db.Exec(query, content); err != nil {
         fmt.Println(err)
         return c.String(http.StatusInternalServerError, "保存までにエラーが発生しました")
     }
